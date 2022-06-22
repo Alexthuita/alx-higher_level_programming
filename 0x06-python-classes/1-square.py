b9632a1b1e33508933df9f21e30348ba972ec178
Alexthuita <alexthuita1@gmail.com>
2022-06-22 13:49:27 +0000

    new fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+class Square():
+    """Class representing a square"""
 
-class Square:
-    
     def __init__(self, size):
-        
+        """ initialize the class
+        Args:
+           size (int): size of the square
+        """
         self.__size = size
-
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 class Square():
+    """square class with it's size and proper validation"""
 
     def __init__(self, size=0):
         if (type(size) is not int):
@@ -8,3 +9,4 @@ class Square():
         elif (size < 0):
             raise ValueError("size must be >= 0")
         self.__size = size
+
